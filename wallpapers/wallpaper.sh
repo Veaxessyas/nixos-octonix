10a435084d72029234575ba653f1f843671f4bc7
Veaxess <147938888+Veaxessyas@users.noreply.github.com>
2026-03-31 21:36:56 +0000

    Add files via upload

---
+++ wallpapers/wallpaper.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# Wallpaper directory
+WALLPAPER_DIR="$HOME/Pictures/wallpapers/hyprpaper"
+
+# Grab all image paths
+WALLPAPERS=($(find "$WALLPAPER_DIR" -type f \( -iname "*.jpg" -o -iname "*.png" \)))
+
+# Choose a random one to start with
+RANDOM_WALLPAPER="${WALLPAPERS[RANDOM % ${#WALLPAPERS[@]}]}"
+
+# Wait until hyprpaper is ready
+until hyprctl hyprpaper listpreloads &>/dev/null; do
+    sleep 0.2
+done
+
+# Start hyprpaper with that random wallpaper
+hyprctl hyprpaper unload all
+hyprctl hyprpaper preload "$RANDOM_WALLPAPER"
+hyprctl hyprpaper wallpaper "eDP-1,$RANDOM_WALLPAPER"
+
+sleep 900
+
+# Then loop and rotate every 30 minutes
+while true; do
+    for WP in "${WALLPAPERS[@]}"; do
+        hyprctl hyprpaper unload all
+        hyprctl hyprpaper preload "$WP"
+        hyprctl hyprpaper wallpaper "eDP-1,$WP"
+        sleep 900
+    done
+done
